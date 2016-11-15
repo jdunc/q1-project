7722dbbd06c6629c721993b7f7e92f26e0065cfa
Jordan Duncan <j.duncan@rochester.edu>
2016-11-15 15:28:35 -0600

    alchemy work

--- script.js
+++ script.js
@@ -36,15 +36,40 @@ $(document).ready(function(){
       console.log($search);
       //make call to bing news search
       $.ajax({
-        url: `https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=${$search}&count=10&offset=0&mkt=en-us&safeSearch=Moderate HTTP/1.1`,
-        data: {
-          'Ocp-Apim-Subscription-Key': '8540eb4d23b7423ba14487c5b24b50e7',
-          'host': 'api.cognitive.microsoft.com',
+        url: `https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=${$search}&count=10&offset=0&mkt=en-us&safeSearch=Moderate`,
+        // beforeSend: function(xhr){xhr.setRequestHeader('Ocp-Apim-Subscription-Key', '8540eb4d23b7423ba14487c5b24b50e7');},
+        headers: {
+          'Ocp-Apim-Subscription-Key': '8540eb4d23b7423ba14487c5b24b50e7'
         },
-        cache: false,
+        dataType: 'json',
         type: "GET",
         success: function(response) {
           console.log(response);
+          appendResults(response);
+          console.log(response.value[0].url);
+          var url = response.value[0].url;
+          $.ajax({
+              type: "GET",
+              url: `
+http://galvanize-cors-proxy.herokuapp.com/${url}`,
+              success: function(data, status, xhr) {
+                  var location = xhr.getResponseHeader('Location');
+                  console.log('location', location);
+                  console.log(data);
+                  console.log(xhr);
+              }
+          });
+          $.ajax({
+            url: `https://watson-api-explorer.mybluemix.net/alchemy-api/calls/url/URLGetCombinedData?apikey=c1a9930232197e836a43483c82066f757e11235a&url=${url}&outputMode=json&extract=doc-emotion,entities`,
+            dataType: 'json',
+            type: "POST",
+            success: function(data) {
+              console.log(data);
+            },
+            error: function(xhr) {
+              console.log('error',xhr);
+            }
+          });
         },
         error: function(xhr) {
           console.log('error',xhr);
@@ -53,3 +78,18 @@ $(document).ready(function(){
     } //end of onclick function
   )//end of on clicking search
 }) //end of on document ready
+function appendResults(results){
+  for (var i = 0; i < results.value.length; i++) {
+    $('.results').append(`
+      <div class="row">
+        <div class="article col-sm-8">
+          <h1><a href="${results.value[i].url}">${results.value[i].name}</a></h1>
+          <p>${results.value[i].description}</p>
+        </div>
+        <div class="articleImage col-sm-4" >
+        </div>
+      </div>
+      `)
+      // <img src="${results.value[i].image.thumbnail.contentUrl}" />
+  }
+}
